274189d6a0b5b745ec80dbfa4ffbcc80f1b5f927
Shakil Ahmad <ahmad.shakil444@gmail.com>
2025-06-06 14:43:47 +0530

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Full Stack Todo App</h1>
+      <p>Designed and mantained by Sandeep, Sahil</p>
+    </>
+  )
+}
+
+export default App
